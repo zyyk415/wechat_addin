2ffe32378f71a9e59e6aa96f870ae94b91d6c9b6
zyyk415 <zyyk415@163.com>
2018-11-27 14:49:29 +0800

    修改了时间的处理和写入文件的处理

--- autoAnswerGrup.py
+++ autoAnswerGrup.py
@@ -5,6 +5,13 @@ from itchat.content import *
 import re
 import ctypes  
 import time
+import os
+import sys
+
+pathFloder = os.path.dirname(__file__)
+DllFilePath = os.path.join(pathFloder,"crptyOK.dll")
+#f = open(pathFloder+"\\"+"log.txt","w")
+WechatGroupname = "AI"
 
 #@itchat.msg_register([PICTURE,TEXT],isGroupChat=True)
 @itchat.msg_register([PICTURE,TEXT],isGroupChat=True)
@@ -18,7 +25,7 @@ def simple_reply(msg):
        if len(devid) == 8:     
            devid_b = devid.encode("utf-8")  
            #确定需要发送的群
-           rooms = itchat.search_chatrooms("test")
+           rooms = itchat.search_chatrooms(WechatGroupname)
            userName = rooms[0]['UserName']
            print (userName)
 #           print (msg['ActualNickName'])
@@ -30,11 +37,14 @@ def simple_reply(msg):
            strkey = b"000000"
            #调用C库
            so = ctypes.cdll.LoadLibrary   
-           lib = so("F:\\03_src\\python source\\crptyOK.dll")   
+           lib = so(DllFilePath)
            lib.get_lock_code(strin,strkey)
            print(strkey) #最终的key
            #发送微信到专门的群上
            itchat.send('@%s %s '%(msg['ActualNickName'],strkey.decode()),toUserName=userName)
 #           itchat.send('%s'%(strkey),toUserName=userName)
+           f = open(pathFloder+"\\"+"log.txt","w")
+           f.write(msg['ActualNickName'] +" "+ nowtime + "\n")
+           f.close()
 itchat.auto_login()#enableCmdQR=True 可以在命令行显示二维码
 itchat.run()
