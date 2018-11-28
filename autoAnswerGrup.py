d00f76672602beb62a37d65ed620d639343d4f30
zyyk415 <zyyk415@163.com>
2018-11-28 17:15:19 +0800

    增加写入文件操作

--- autoAnswerGrup.py
+++ autoAnswerGrup.py
@@ -11,7 +11,9 @@ import sys
 pathFloder = os.path.dirname(__file__)
 DllFilePath = os.path.join(pathFloder,"crptyOK.dll")
 #f = open(pathFloder+"\\"+"log.txt","w")
+#只需要修改下面的群名就可以了
 WechatGroupname = "AI"
+No = 0
 
 #@itchat.msg_register([PICTURE,TEXT],isGroupChat=True)
 @itchat.msg_register([PICTURE,TEXT],isGroupChat=True)
@@ -22,7 +24,9 @@ def simple_reply(msg):
     if msg['Content'][0:5] == "求设备密码":
        #取出设备号
        devid = re.sub("\D", "", msg['Content'])
-       if len(devid) == 8:     
+       if len(devid) == 8:
+           global No
+           No = No + 1
            devid_b = devid.encode("utf-8")  
            #确定需要发送的群
            rooms = itchat.search_chatrooms(WechatGroupname)
@@ -31,6 +35,7 @@ def simple_reply(msg):
 #           print (msg['ActualNickName'])
            #取出当前时间
            nowtime=time.strftime('%Y%m%d',time.localtime(time.time()))
+           nowtime_ymdhms = time.strftime('%Y-%m-%d %H:%m',time.localtime(time.time()))
            nowtime_b = nowtime.encode("utf-8")
            #输入输出参数
            strin = devid_b+nowtime_b
@@ -43,8 +48,8 @@ def simple_reply(msg):
            #发送微信到专门的群上
            itchat.send('@%s %s '%(msg['ActualNickName'],strkey.decode()),toUserName=userName)
 #           itchat.send('%s'%(strkey),toUserName=userName)
-           f = open(pathFloder+"\\"+"log.txt","w")
-           f.write(msg['ActualNickName'] +" "+ nowtime + "\n")
+           f = open(pathFloder+"\\"+"log.txt","a+")
+           f.write(str(No)+" "+msg['ActualNickName'] +" "+ nowtime_ymdhms + "\n")
            f.close()
 itchat.auto_login()#enableCmdQR=True 可以在命令行显示二维码
 itchat.run()
