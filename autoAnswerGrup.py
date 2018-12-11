2dd93fa6f2682d1810b9ba7dce4c029e7c82d9b9
zyyk415 <zyyk415@163.com>
2018-12-11 13:21:46 +0800

    1.增加群名的打印 2.增加异常的处理

--- autoAnswerGrup.py
+++ autoAnswerGrup.py
@@ -7,47 +7,54 @@ import ctypes
 import time
 import os
 import sys
+import logging
 
 #只需要修改下面的群名就可以了
-WechatGroupname = "AI"
+WechatGroupname = "驾培旗舰版授权"
 No = 0
 
 pathFloder = os.path.dirname(__file__)
 DllFilePath = os.path.join(pathFloder,"crpty.dll")
 
-#@itchat.msg_register([PICTURE,TEXT],isGroupChat=True)
-@itchat.msg_register([PICTURE,TEXT],isGroupChat=True)
+#@itchat.msg_register([TEXT],isGroupChat=True)
+@itchat.msg_register([TEXT],isGroupChat=True)
 def simple_reply(msg):
     #匹配需要回复的关键字
     #【求设备密码12345678】
-    print (msg['Content'])
-    if msg['Content'][0:5] == "求设备密码":
-       #取出设备号
-       devid = re.sub("\D", "", msg['Content'])
-       if len(devid) == 8:
-           global No
-           No = No + 1
-           devid_b = devid.encode("utf-8")  
-           #确定需要发送的群
-           rooms = itchat.search_chatrooms(WechatGroupname)
-           userName = rooms[0]['UserName']
-           print (userName)
-           #取出当前时间
-           nowtime=time.strftime('%Y%m%d',time.localtime(time.time()))
-           nowtime_ymdhms = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-           nowtime_b = nowtime.encode("utf-8")
-           #输入输出参数
-           strin = devid_b+nowtime_b
-           strkey = b"000000"
-           #调用C库
-           so = ctypes.cdll.LoadLibrary   
-           lib = so(DllFilePath)
-           lib.get_lock_code(strin,strkey)
-           print(strkey) #最终的key
-           #发送微信到专门的群上
-           itchat.send('@%s %s '%(msg['ActualNickName'],strkey.decode()),toUserName=userName)
-           f = open(pathFloder+"\\"+"log.txt","a+")
-           f.write(str(No)+" "+msg['ActualNickName'] +" "+ nowtime_ymdhms +" "+ devid + "\n")
-           f.close()
+    try:
+        print ("消息发送群 " + msg['User']['NickName'])
+        print ("消息发送人  "+ msg['ActualNickName'])
+        print (msg['Content']+"\n")
+        if msg['Content'][0:5] == "求设备密码":
+           #取出设备号
+           devid = re.sub("\D", "", msg['Content'])
+           if len(devid) == 8:
+               global No
+               No = No + 1
+               devid_b = devid.encode("utf-8")  
+               #确定需要发送的群
+               rooms = itchat.search_chatrooms(WechatGroupname)
+               userName = rooms[0]['UserName']
+               print (userName)
+               #取出当前时间
+               nowtime=time.strftime('%Y%m%d',time.localtime(time.time()))
+               nowtime_ymdhms = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+               nowtime_b = nowtime.encode("utf-8")
+               #输入输出参数
+               strin = devid_b+nowtime_b
+               strkey = b"000000"
+               #调用C库
+               so = ctypes.cdll.LoadLibrary   
+               lib = so(DllFilePath)
+               lib.get_lock_code(strin,strkey)
+               print(strkey) #最终的key
+               print("\n")
+               #发送微信到专门的群上
+               itchat.send('@%s %s '%(msg['ActualNickName'],strkey.decode()),toUserName=userName)
+               f = open(pathFloder+"\\"+"log.txt","a+")
+               f.write(str(No)+" "+msg['ActualNickName'] +" "+ nowtime_ymdhms +" "+ devid + "\n")
+               f.close()
+    except Exception as e:
+        logging.exception(e)
 itchat.auto_login()#enableCmdQR=True 可以在命令行显示二维码
 itchat.run()
