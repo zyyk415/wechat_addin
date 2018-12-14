c836f4ee1c668a483d65e00994d28c64bdf56d24
zyyk415 <zyyk415@163.com>
2018-12-15 00:39:10 +0800

    增加命令行可以回复的功能

---
+++ chat.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+#coding=UTF-8
+import itchat
+from itchat.content import *
+import re
+import ctypes  
+import time
+import os
+import sys
+import logging
+import threading
+
+#只需要修改下面的群名就可以了
+WechatGroupname = "驾培旗舰版授权"
+No = 0
+
+#取得当前文件的目录
+pathFloder = os.path.dirname(__file__)
+DllFilePath = os.path.join(pathFloder,"crpty.dll")
+
+non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
+
+#@itchat.msg_register([TEXT],isFriendChat=True,isGroupChat=True)
+@itchat.msg_register([PICTURE,TEXT],isGroupChat=True)
+def simple_reply(msg):
+    global non_bmp_map
+    #匹配需要回复的关键字
+    #【求设备密码12345678】
+    try:
+        print (msg['User']['NickName'].translate(non_bmp_map)+" ",end="")
+        print (msg['ActualNickName'].translate(non_bmp_map))
+        print (msg['Content'].translate(non_bmp_map))
+        if msg['Content'][0:5] == "求设备密码":
+           #取出设备号
+           devid = re.sub("\D", "", msg['Content'])
+           if len(devid) == 8:
+               global No
+               No = No + 1
+               devid_b = devid.encode("utf-8")  
+               #确定需要发送的群
+               rooms = itchat.search_chatrooms(WechatGroupname)
+               userName = rooms[0]['UserName']
+#               print (userName)
+               #取出当前时间
+               nowtime=time.strftime('%Y%m%d',time.localtime(time.time()))
+               nowtime_ymdhms = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+               nowtime_b = nowtime.encode("utf-8")
+               #输入输出参数
+               strin = devid_b+nowtime_b
+               strkey = b"000000"
+               #调用C库
+               so = ctypes.cdll.LoadLibrary   
+               lib = so(DllFilePath)
+               lib.get_lock_code(strin,strkey)
+               #发送微信到专门的群上
+               itchat.send('@%s %s '%(msg['ActualNickName'],strkey.decode()),toUserName=userName)
+               print('@%s %s '%(msg['ActualNickName'].translate(non_bmp_map),strkey.decode()))
+               f = open(pathFloder+"\\"+"log.txt","a+")
+               f.write(str(No)+" "+msg['ActualNickName'] +" "+ nowtime_ymdhms +" "+ devid + "\n")
+               f.close()
+    except Exception as e:
+        logging.exception(e)
+
+#接收朋友的消息
+@itchat.msg_register([PICTURE,TEXT],isFriendChat=True)
+def simple_friend_reply(msg):
+    global non_bmp_map
+    try:
+        print (msg['User']['NickName'].translate(non_bmp_map)+" ",end="")
+#        print (msg['ActualNickName'].translate(non_bmp_map))
+        print (msg['Content'].translate(non_bmp_map))
+    except Exception as e:
+        logging.exception(e)    
+
+#@itchat.msg_register([TEXT],isFriendChat=True,isGroupChat=True)
+#定义回复函数，回复是，先输入想要回复的人或群，然后输入一个空格，再输入回复消息即可回复。
+def mes_select_reply():
+    while(1):
+        try:
+            time.sleep(3)
+            replymessage=""
+            replymessage= input("输入对象 内容\n")
+            #想要回复的对象 （群名或好友名称）
+            name0 = replymessage.split()[0]
+            #通过好友的昵称找到username
+            userName0 = itchat.search_friends(name=name0)
+            if userName0 != []:
+                userName = userName0[0]["UserName"]
+            else: 
+                #通过群名找到username
+                rooms = itchat.search_chatrooms(name0)
+                if rooms != []:
+                    userName = rooms[0]['UserName']
+                else:
+                    continue  
+            #想要回复的内容
+            content =  replymessage.split(" ",1)[1]
+            print("@" + name0 +" "+content+"\n" )
+            itchat.send("%s"%(content),toUserName=userName)
+        except Exception as e:
+            logging.exception(e) 
+
+def itdo():
+    itchat.auto_login()#enableCmdQR=True 可以在命令行显示二维码
+    itchat.run()
+    
+t = threading.Thread(target=mes_select_reply) #开启并行线程
+#t.setDaemon(True)
+t.start()
+t1 = threading.Thread(target=itdo) #开启并行线程
+#t.setDaemon(True)
+t1.start()
+
+
+
