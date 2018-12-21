74d76ece53da10d68c13eb1a20c45385d89ad009
zyyk415 <zyyk415@163.com>
2018-12-21 19:17:06 +0800

    1.增加群图片按照群名文件夹保存

--- chat.py
+++ chat.py
@@ -31,16 +31,16 @@ def simple_reply(msg):
         nowtime=time.strftime('%Y%m%d',time.localtime(time.time()))
         nowtime_ymdhms = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
         nowtime_b = nowtime.encode("utf-8")
-        print (msg['User']['NickName'].translate(non_bmp_map)+" ",end="")
         print (msg['ActualNickName'].translate(non_bmp_map),end="")
+        print (msg['User']['NickName'].translate(non_bmp_map)+" ",end="")
         print(" " + nowtime_ymdhms)
         print (msg['Content'].translate(non_bmp_map)+"\n")
         #将聊天记录记录到各自的文件中
         f = open(pathFloder+"\\"+msg['User']['NickName'].translate(non_bmp_map)+".txt","a+",encoding='utf-8')
-        f.write(msg['User']['NickName'].translate(non_bmp_map))
-        f.write(msg['ActualNickName'].translate(non_bmp_map))
-        f.write(" " + nowtime_ymdhms)
-        f.write(msg['Content'].translate(non_bmp_map)+"\n")
+        f.write(msg['ActualNickName'].translate(non_bmp_map)+" ")
+        f.write(" " + nowtime_ymdhms+" ")
+        f.write(msg['User']['NickName'].translate(non_bmp_map)+"\n")
+        f.write(msg['Content'].translate(non_bmp_map)+"\n"+"\n")
         f.close()
 
         if msg['Content'][0:5] == "求设备密码":
@@ -82,28 +82,24 @@ def simple_friend_reply(msg):
         logging.exception(e)    
 
 #接收朋友的图片信息
-#@itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
-#def download_files(msg):
-#    try:
-#        msg.download(os.path.join(pathFloder,msg.fileName))
-#        typeSymbol = {
-#            PICTURE: 'img',
-#            VIDEO: 'vid', }.get(msg.type, 'fil')
-#        return '@%s@%s' % (typeSymbol, msg.fileName)
-#    except Exception as e:
-#        logging.exception(e)  
-#
+@itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
+def download_files(msg):
+    try:
+        path = os.path.join(pathFloder,msg['User']['NickName'].translate(non_bmp_map))
+        makedir(path)
+        msg.download(os.path.join(path,msg.fileName))
+    except Exception as e:
+        logging.exception(e)  
+
 #接收群的图片信息
-#@itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO],isGroupChat=True)
-#def download_files_frome_group(msg):
-#    try:
-#        msg.download(os.path.join(pathFloder,msg.fileName))
-#        typeSymbol = {
-#            PICTURE: 'img',
-#            VIDEO: 'vid', }.get(msg.type, 'fil')
-#        return '@%s@%s' % (typeSymbol, msg.fileName)
-#    except Exception as e:
-#        logging.exception(e) 
+@itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO],isGroupChat=True)
+def download_files_frome_group(msg):
+    try:
+        path = os.path.join(pathFloder,msg['User']['NickName'].translate(non_bmp_map))
+        makedir(path)
+        msg.download(os.path.join(path,msg.fileName))
+    except Exception as e:
+        logging.exception(e) 
 
 #@itchat.msg_register([TEXT],isFriendChat=True,isGroupChat=True)
 #定义回复函数，回复是，先输入想要回复的人或群，然后输入一个空格，再输入回复消息即可回复。
@@ -132,7 +128,25 @@ def mes_select_reply():
             itchat.send("%s"%(content),toUserName=userName)
         except Exception as e:
             logging.exception(e) 
-
+#创建文件夹
+def makedir(path):
+    # 去除首位空格
+    path=path.strip()
+    # 去除尾部 \ 符号
+    path=path.rstrip("\\")
+    isExists=os.path.exists(path)
+     # 判断结果
+    if not isExists:
+        # 如果不存在则创建目录
+        # 创建目录操作函数
+        os.makedirs(path) 
+        print (path+' 创建成功')
+        return True
+    else:
+        # 如果目录存在则不创建，并提示目录已存在
+#       print (path+' 目录已存在')
+        return False
+#登录微信
 def itdo():
     itchat.auto_login()#enableCmdQR=True 可以在命令行显示二维码
     itchat.run()
@@ -143,6 +157,7 @@ t.start()
 t1 = threading.Thread(target=itdo) #开启并行线程
 #t.setDaemon(True)
 t1.start()
+
 
 
 
