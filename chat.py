c5b4538a81883c666774d040390decfb988e0d3d
zyyk415 <zyyk415@163.com>
2018-12-25 12:53:21 +0800

    增加输入框和回车功能 增加@我的内容标红

--- chat.py
+++ chat.py
@@ -10,6 +10,7 @@ import sys
 import logging
 import threading
 import tkinter as tk
+import color
 
 #只需要修改下面的群名就可以了
 WechatGroupname = "驾培旗舰版授权"
@@ -32,10 +33,16 @@ def simple_reply(msg):
         nowtime=time.strftime('%Y%m%d',time.localtime(time.time()))
         nowtime_ymdhms = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
         nowtime_b = nowtime.encode("utf-8")
-        print (msg['ActualNickName'].translate(non_bmp_map),end="")
-        print (msg['User']['NickName'].translate(non_bmp_map)+" ",end="")
-        print(" " + nowtime_ymdhms)
-        print (msg['Content'].translate(non_bmp_map)+"\n")
+        if msg["isAt"]:
+            clr.print_red_text (msg['ActualNickName'].translate(non_bmp_map))
+            clr.print_red_text (msg['User']['NickName'].translate(non_bmp_map)+" ")
+            clr.print_red_text(" " + nowtime_ymdhms)
+            clr.print_red_text (msg['Content'].translate(non_bmp_map)+"\n")           
+        else:
+            print (msg['ActualNickName'].translate(non_bmp_map))
+            print (msg['User']['NickName'].translate(non_bmp_map)+" ",end="")
+            print(" " + nowtime_ymdhms)
+            print (msg['Content'].translate(non_bmp_map)+"\n")
         #将聊天记录记录到各自的文件中
         f = open(pathFloder+"\\"+msg['User']['NickName'].translate(non_bmp_map)+".txt","a+",encoding='utf-8')
         f.write(msg['ActualNickName'].translate(non_bmp_map)+" ")
@@ -75,9 +82,9 @@ def simple_reply(msg):
 def simple_friend_reply(msg):
     global non_bmp_map
     try:
-        print (msg['User']['NickName'].translate(non_bmp_map)+" ",end="")
+        clr.print_red_text(msg['User']['NickName'].translate(non_bmp_map)+" ")
 #        print (msg['ActualNickName'].translate(non_bmp_map))
-        print (msg['Content'].translate(non_bmp_map))
+        clr.print_red_text(msg['Content'].translate(non_bmp_map))
     except Exception as e:
         logging.exception(e)    
 
@@ -103,10 +110,11 @@ def download_files_frome_group(msg):
 
 #@itchat.msg_register([TEXT],isFriendChat=True,isGroupChat=True)
 #定义回复函数，回复是，先输入想要回复的人或群，然后输入一个空格，再输入回复消息即可回复。
-def mes_select_reply():
+def mes_select_reply(event):
     try:
         replymessage=""
         replymessage= E1.get()
+        E1.delete(0, tk.END)  # 删除所有值
         #想要回复的对象 （群名或好友名称）
         name0 = replymessage.split()[0]
         #通过好友的昵称找到username
@@ -153,14 +161,22 @@ t1 = threading.Thread(target=itdo) #开启并行线程
 #t.setDaemon(True)
 t1.start()
 
+#设置颜色
+clr = color.Color()
+clr.print_red_text('red')
+
+
 #创建界面
 chatframe = tk.Tk("消息回复")
 #chatframe.geometry("500x50")
 E1 = tk.Entry(chatframe,width=65)
+#给输入框绑定按键监听事件<Key>为监听任何按键 <Key-x>监听其它键盘，如大写的A<Key-A>、回车<Key-Return>
+E1.bind('<Key-Return>', mes_select_reply)
 E1.pack(side = tk.LEFT)
 B1 = tk.Button(chatframe,text = "发送",command = mes_select_reply)
 B1.pack(side = tk.RIGHT)
 chatframe.mainloop()
+
 
 
 
